9a53d0df778f0e8c54fc51ba4afffbafc2d50941
Alex Chindris <chindrisalex@yahoo.ro>
2021-11-27 15:25:31 +0200

    -

---
+++ Data/Chindris_Alexandru_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Chindris_Alexandru_Lab8.Models;
+
+namespace Chindris_Alexandru_Lab8.Data
+{
+    public class Chindris_Alexandru_Lab8Context : DbContext
+    {
+        public Chindris_Alexandru_Lab8Context (DbContextOptions<Chindris_Alexandru_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Chindris_Alexandru_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Chindris_Alexandru_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Chindris_Alexandru_Lab8.Models.Category> Category { get; set; }
+    }
+}
